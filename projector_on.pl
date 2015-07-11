95edca554ebe0b775dffb505c5042c33c2369fa0
Dave Jacoby <jacoby.david@gmail.com>
2015-07-11 19:03:42 -0400

    Created, Added Code, Documented
    added the projector_on and projector_off programs

---
+++ projector_on.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+# ------------------------------------------------------------
+# projector_turn_on.pl
+# ------------------------------------------------------------
+use feature qw{say} ;
+use utf8 ;
+use Net::PJLink qw{ :RESPONSES } ;
+
+my $pass = 'admin' ;
+my $projectors = [ '172.17.4.10' ] ;
+my $prj = Net::PJLink->new( host => $projectors , keep_alive => 1 ) ;
+$prj->set_auth_password( $pass ) ;
+$prj->set_power( 1 ) ;
+$prj->close_all_connections;
+exit;
